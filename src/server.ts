529e70b06741064ca12bef66f47e069edae1576e
Francisco Groppo <groppofilho@yahoo.com.br>
2022-05-08 01:33:58 -0300

    Add port

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, "0.0.0.0", () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP Server running");
 });
