8be7d84091cd91189f08ce59e1e8ab836cb3d298
Francisco Groppo <groppofilho@yahoo.com.br>
2022-05-08 01:08:25 -0300

    Final NLW class

---
+++ Readme.md
@@ -0,0 +1,9 @@
+How to run
+
+# npm i
+
+# npm i -D prisma
+
+# npx prisma generate
+
+# npm run dev
---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "3ecc1ff2fd5993",
+    pass: "10442e87d07551",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Francisco Groppo <groppofilho@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
